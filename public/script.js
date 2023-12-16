c0f4b7a75c1759c7fa336b3c38f264b2c136c5c7
TEJI <jet@teji.io>
2023-12-16 18:39:52 +1100

    save for now

--- public/script.js
+++ public/script.js
@@ -1,65 +1,71 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const socket = io(); // Connect to the WebSocket server
+    const socket = io();
     
     const messageInput = document.getElementById("message-input");
     const sendButton = document.getElementById("send-button");
     const chatContainer = document.getElementById("chat-container");
-    const timerValue = document.getElementById("timer-value");
+    const roomInfo = document.getElementById("room-info");
     
     function appendMessage(username, message, color) {
-        const chatMessage = document.createElement("div");
-        chatMessage.classList.add("chat-message");
-        chatMessage.innerHTML = `<span class="username" style="color: ${color};">${username}:</span> ${message}`;
-        chatContainer.appendChild(chatMessage);
-        chatContainer.scrollTop = chatContainer.scrollHeight;
+    const chatMessage = document.createElement("div");
+    chatMessage.classList.add("chat-message");
+    chatMessage.innerHTML = `<span class="username" style="color: ${color};">${username}:</span> ${message}`;
+    chatContainer.appendChild(chatMessage);
+    chatContainer.scrollTop = chatContainer.scrollHeight;
     }
     
     function sendMessage() {
-        const message = messageInput.value.trim();
-        if (message !== "") {
-            appendMessage("You", message, "#007bff");
-            socket.emit("chat message", { username: "You", message, color: "#007bff" });
-            messageInput.value = "";
-        }
+    const message = messageInput.value.trim();
+    if (message !== "") {
+    appendMessage("You", message, "#007bff");
+    socket.emit("chat message", { username: "You", message, color: "#007bff" });
+    messageInput.value = "";
+    }
     }
     
     sendButton.addEventListener("click", () => {
-        sendMessage();
+    sendMessage();
     });
     
     messageInput.addEventListener("keydown", (event) => {
-        if (event.key === "Enter") {
-            sendMessage();
-        }
+    if (event.key === "Enter") {
+    sendMessage();
+    }
     });
     
     socket.on("chat message", (data) => {
-        appendMessage(data.username, data.message, data.color);
+    appendMessage(data.username, data.message, data.color);
     });
     
     socket.on('room assigned', (data) => {
-        console.log(`Assigned to room: ${data.room} with duration ${data.duration}`);
-        startTimer(data.duration);
+    console.log(`Assigned to room: ${data.room} with duration ${data.duration}`);
+    roomInfo.innerHTML = `Room: ${data.room} | Duration: ${Math.floor(data.duration / 60)} minutes`;
+    
+    // Initialize the countdown timer
+    startTimer(data.duration);
     });
     
+    // Function to handle the countdown timer
     function startTimer(duration) {
-        let minutes = Math.floor(duration / 60);
-        let seconds = duration % 60;
+    let minutes = Math.floor(duration / 60);
+    let seconds = duration % 60;
     
-        const timerInterval = setInterval(function() {
-            seconds--;
-            if (seconds < 0) {
-                minutes--;
-                seconds = 59;
-            }
+    const timerElement = document.getElementById("timer");
+    timerElement.innerHTML = `${minutes} minutes and ${seconds} seconds`;
     
-            timerValue.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+    const interval = setInterval(() => {
+    if (duration > 0) {
+    minutes = Math.floor(duration / 60);
+    seconds = duration % 60;
     
-            if (minutes <= 0 && seconds <= 0) {
-                clearInterval(timerInterval);
-                alert('Your chat session has ended.');
-                window.location.reload(); // Reload the page to start a new chat session
-            }
-        }, 1000);
+    timerElement.innerHTML = `${minutes} minutes and ${seconds} seconds`;
+    
+    duration--;
+    } else {
+    clearInterval(interval);
+    // Redirect user to a new room when the timer ends
+    window.location.reload();
     }
-});
+    }, 1000);
+    }
+    })
--- server.js
+++ server.js
@@ -9,70 +9,87 @@ const io = socketIo(server);
 
 const port = process.env.PORT || 3001;
 
-let chatRooms = {};
-let roomDurations = {};
+// Initialize chatRooms and roomDurations with default values
+let chatRooms = {
+'room-1': []
+};
+let roomDurations = {
+'room-1': 600
+};
+
 let connectedUsers = {};
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', (socket) => {
-    console.log('A user connected');
+console.log('A user connected');
 
-    const roomName = assignUserToRoom(socket.id);
-    socket.join(roomName);
-    connectedUsers[socket.id] = { room: roomName };
+const roomName = assignUserToRoom(socket.id);
+socket.join(roomName);
+connectedUsers[socket.id] = { room: roomName };
 
-    console.log(`User ${socket.id} assigned to room ${roomName}`);
+console.log(`User ${socket.id} assigned to room ${roomName}`);
 
-    socket.emit('chat message', {
-        username: 'CommonRoom',
-        text: 'Welcome to CommonRoom! Start chatting with random people from around the world.',
-        timestamp: moment().valueOf(),
-        color: "#000000"
-    });
+// Emit a welcome message to the user who just joined
+socket.emit('chat message', {
+username: 'CommonRoom',
+text: 'Welcome to CommonRoom! Start chatting with random people from around the world.',
+timestamp: moment().valueOf(),
+color: "#000000"
+});
 
-    socket.on('chat message', (data) => {
-        console.log(`Received message in room ${roomName}:`, data);
-        io.to(roomName).emit('chat message', data);
-    });
+// Notify others in the room
+socket.broadcast.to(roomName).emit('chat message', {
+username: 'CommonRoom',
+text: `A new user has joined the chat.`,
+timestamp: moment().valueOf(),
+color: "#000000"
+});
 
-    socket.on('disconnect', () => {
-        console.log(`User ${socket.id} disconnected`);
-        if (connectedUsers[socket.id]) {
-            delete connectedUsers[socket.id];
-        }
-        removeFromRoom(socket.id, roomName);
-    });
+socket.emit('room assigned', { room: roomName, duration: roomDurations[roomName] });
+
+socket.on('chat message', (data) => {
+console.log(`Received message in room ${roomName}:`, data);
+io.to(roomName).emit('chat message', data);
+});
+
+// Add a listener for the 'disconnect' event
+socket.on('disconnect', () => {
+console.log(`User ${socket.id} disconnected`);
+removeFromRoom(socket.id, roomName);
+delete connectedUsers[socket.id];
+});
 });
 
 function assignUserToRoom(socketId) {
-    for (const room in chatRooms) {
-        if (chatRooms[room].length < 5) {
-            chatRooms[room].push(socketId);
-            return room;
-        }
-    }
-
-    const newRoom = `room-${Object.keys(chatRooms).length + 1}`;
-    chatRooms[newRoom] = [socketId];
-    roomDurations[newRoom] = Math.floor(Math.random() * (60 * 60)) + 60;
-    return newRoom;
+for (const room in chatRooms) {
+if (chatRooms[room].length < 5) {
+chatRooms[room].push(socketId);
+return room;
+}
+}
+const newRoom = `room-${Object.keys(chatRooms).length + 1}`;
+chatRooms[newRoom] = [socketId];
+roomDurations[newRoom] = Math.floor(Math.random() * (60 * 60)) + 60;
+return newRoom;
 }
 
 function removeFromRoom(socketId, roomName) {
-    chatRooms[roomName] = chatRooms[roomName].filter(id => id !== socketId);
-    delete connectedUsers[socketId];
+chatRooms[roomName] = chatRooms[roomName].filter(id => id !== socketId);
+if (chatRooms[roomName].length === 0) {
+cleanupRoom(roomName);
+}
+}
 
-    if (chatRooms[roomName].length === 0) {
-        delete chatRooms[roomName];
-        delete roomDurations[roomName];
-    }
+function cleanupRoom(roomName) {
+delete chatRooms[roomName];
+delete roomDurations[roomName];
 }
 
 server.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+console.log(`Server running on http://localhost:${port}`);
 });
