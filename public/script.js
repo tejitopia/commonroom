bd468903409352cdeaa7ef960d26260ed19a6945
abd038 <mohammadabdulrehman739@gmail.com>
2023-12-16 15:34:14 +0530

    user connecting, leaving counts corrected

--- public/script.js
+++ public/script.js
@@ -1,71 +1,108 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const socket = io();
-    
-    const messageInput = document.getElementById("message-input");
-    const sendButton = document.getElementById("send-button");
-    const chatContainer = document.getElementById("chat-container");
-    const roomInfo = document.getElementById("room-info");
-    
-    function appendMessage(username, message, color) {
+  const socket = io();
+
+  const messageInput = document.getElementById("message-input");
+  const sendButton = document.getElementById("send-button");
+  const chatContainer = document.getElementById("chat-container");
+  const roomInfo = document.getElementById("room-info");
+
+  function appendMessage(username, message, color) {
     const chatMessage = document.createElement("div");
     chatMessage.classList.add("chat-message");
     chatMessage.innerHTML = `<span class="username" style="color: ${color};">${username}:</span> ${message}`;
     chatContainer.appendChild(chatMessage);
     chatContainer.scrollTop = chatContainer.scrollHeight;
-    }
-    
-    function sendMessage() {
+  }
+
+  function sendMessage() {
     const message = messageInput.value.trim();
     if (message !== "") {
-    appendMessage("You", message, "#007bff");
-    socket.emit("chat message", { username: "You", message, color: "#007bff" });
-    messageInput.value = "";
+      appendMessage("You", message, "#007bff");
+      socket.emit("chat message", {
+        username: "You",
+        message,
+        color: "#007bff",
+      });
+      messageInput.value = "";
     }
-    }
-    
-    sendButton.addEventListener("click", () => {
+  }
+
+  sendButton.addEventListener("click", () => {
     sendMessage();
-    });
-    
-    messageInput.addEventListener("keydown", (event) => {
+  });
+
+  messageInput.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
-    sendMessage();
+      sendMessage();
     }
-    });
-    
-    socket.on("chat message", (data) => {
+  });
+
+  socket.on("chat message", (data) => {
     appendMessage(data.username, data.message, data.color);
-    });
-    
-    socket.on('room assigned', (data) => {
-    console.log(`Assigned to room: ${data.room} with duration ${data.duration}`);
-    roomInfo.innerHTML = `Room: ${data.room} | Duration: ${Math.floor(data.duration / 60)} minutes`;
-    
-    // Initialize the countdown timer
-    startTimer(data.duration);
-    });
-    
-    // Function to handle the countdown timer
-    function startTimer(duration) {
+  });
+
+  socket.on("user-joined", (data) => {
+    let inputData = JSON.parse(data);
+    console.log(inputData);
+    console.log(`User ${data} joined the chat`);
+
+    startTimer(inputData.time_left);
+    appendMessage(inputData.username, ` joined the chat`, inputData.color);
+    updateUsersCount(inputData.people_in_room);
+    updateOnlineUsersCount(inputData.total_online_users);
+  });
+
+  socket.on("user-left", (data) => {
+    let inputData = JSON.parse(data);
+    console.log(`User ${data} left the chat`);
+    appendMessage(inputData.username, ` left the chat`, inputData.color);
+    updateUsersCount(inputData.people_in_room);
+    updateOnlineUsersCount(inputData.total_online_users);
+  });
+
+  //   socket.on("room assigned", (data) => {
+  //     console.log(
+  //       `Assigned to room: ${data.room} with duration ${data.duration}`
+  //     );
+  //     roomInfo.innerHTML = `Room: ${data.room} | Duration: ${Math.floor(
+  //       data.duration / 60
+  //     )} minutes`;
+
+  //     // Initialize the countdown timer
+  //     startTimer(data.duration);
+  //   });
+
+  // Function to handle the countdown timer
+  function startTimer(duration) {
     let minutes = Math.floor(duration / 60);
     let seconds = duration % 60;
-    
+
     const timerElement = document.getElementById("timer");
     timerElement.innerHTML = `${minutes} minutes and ${seconds} seconds`;
-    
+
     const interval = setInterval(() => {
-    if (duration > 0) {
-    minutes = Math.floor(duration / 60);
-    seconds = duration % 60;
-    
-    timerElement.innerHTML = `${minutes} minutes and ${seconds} seconds`;
-    
-    duration--;
-    } else {
-    clearInterval(interval);
-    // Redirect user to a new room when the timer ends
-    window.location.reload();
-    }
+      if (duration > 0) {
+        minutes = Math.floor(duration / 60);
+        seconds = duration % 60;
+
+        timerElement.innerHTML = `${minutes} minutes and ${seconds} seconds`;
+
+        duration--;
+      } else {
+        clearInterval(interval);
+        // Redirect user to a new room when the timer ends
+        window.location.reload();
+      }
     }, 1000);
-    }
-    })
+  }
+});
+
+function updateUsersCount(count) {
+  const usersCount = document.getElementById("participant-count");
+  usersCount.innerHTML = `👥 ${count}/5 People in Room`;
+}
+
+function updateOnlineUsersCount(count) {
+  const onlineUsersCount = document.getElementById("online-count");
+  onlineUsersCount.innerHTML = `🌐 ${count} Online`;
+}
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const moment = require('moment');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const moment = require("moment");
 
 const app = express();
 const server = http.createServer(app);
@@ -10,86 +10,129 @@ const io = socketIo(server);
 const port = process.env.PORT || 3001;
 
 // Initialize chatRooms and roomDurations with default values
-let chatRooms = {
-'room-1': []
-};
-let roomDurations = {
-'room-1': 600
-};
+let chatRooms = [
+  { id: 1, users: [], duration: Math.ceil(Math.random() * 60) * 60 },
+];
 
-let connectedUsers = {};
-
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-res.sendFile(__dirname + '/public/index.html');
-});
-
-io.on('connection', (socket) => {
-console.log('A user connected');
-
-const roomName = assignUserToRoom(socket.id);
-socket.join(roomName);
-connectedUsers[socket.id] = { room: roomName };
-
-console.log(`User ${socket.id} assigned to room ${roomName}`);
-
-// Emit a welcome message to the user who just joined
-socket.emit('chat message', {
-username: 'CommonRoom',
-text: 'Welcome to CommonRoom! Start chatting with random people from around the world.',
-timestamp: moment().valueOf(),
-color: "#000000"
-});
-
-// Notify others in the room
-socket.broadcast.to(roomName).emit('chat message', {
-username: 'CommonRoom',
-text: `A new user has joined the chat.`,
-timestamp: moment().valueOf(),
-color: "#000000"
-});
-
-socket.emit('room assigned', { room: roomName, duration: roomDurations[roomName] });
-
-socket.on('chat message', (data) => {
-console.log(`Received message in room ${roomName}:`, data);
-io.to(roomName).emit('chat message', data);
-});
-
-// Add a listener for the 'disconnect' event
-socket.on('disconnect', () => {
-console.log(`User ${socket.id} disconnected`);
-removeFromRoom(socket.id, roomName);
-delete connectedUsers[socket.id];
-});
-});
-
-function assignUserToRoom(socketId) {
-for (const room in chatRooms) {
-if (chatRooms[room].length < 5) {
-chatRooms[room].push(socketId);
-return room;
-}
-}
-const newRoom = `room-${Object.keys(chatRooms).length + 1}`;
-chatRooms[newRoom] = [socketId];
-roomDurations[newRoom] = Math.floor(Math.random() * (60 * 60)) + 60;
-return newRoom;
+function totalOnlineUsersCount() {
+  let count = 0;
+  for (let i = 0; i < chatRooms.length; i++) {
+    count += chatRooms[i].users.length;
+  }
+  return count;
 }
 
-function removeFromRoom(socketId, roomName) {
-chatRooms[roomName] = chatRooms[roomName].filter(id => id !== socketId);
-if (chatRooms[roomName].length === 0) {
-cleanupRoom(roomName);
-}
+let connectedUsers = [];
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+io.on("connection", (socket) => {
+  const room = assignUserToRoom(socket.id);
+  socket.join(room.id);
+
+  console.log(`User ${socket.id} assigned to room ${room.id}`);
+
+  io.to(room.id).emit(
+    "user-joined",
+    JSON.stringify({
+      username: socket.id,
+      time_left: room.duration,
+      people_in_room: room.users.length,
+      total_online_users: totalOnlineUsersCount(),
+      color:
+        "#" +
+        ("000000" + Math.floor(Math.random() * 16777215).toString(16)).slice(
+          -6
+        ),
+    })
+  );
+
+  //   // Emit a welcome message to the user who just joined
+  //   socket.emit("chat message", {
+  //     username: "CommonRoom",
+  //     text: "Welcome to CommonRoom! Start chatting with random people from around the world.",
+  //     timestamp: moment().valueOf(),
+  //     color: "#000000",
+  //   });
+
+  // Notify others in the room
+  //   socket.broadcast.to(roomName).emit("chat message", {
+  //     username: "CommonRoom",
+  //     text: `A new user has joined the chat.`,
+  //     timestamp: moment().valueOf(),
+  //     color: "#000000",
+  //   });
+
+  //   socket.emit("room assigned", {
+  //     room: roomName,
+  //     duration: roomDurations[roomName],
+  //   });
+
+  //   socket.on("chat message", (data) => {
+  //     console.log(`Received message in room ${roomName}:`, data);
+  //     io.to(roomName).emit("chat message", data);
+  //   });
+
+  // Add a listener for the 'disconnect' event
+  socket.on("disconnect", () => {
+    console.log(`User ${socket.id} disconnected`);
+    removeFromRoom(socket.id);
+  });
+});
+
+function assignUserToRoom(userSocketId) {
+  for (let i = 0; i < chatRooms.length; i++) {
+    if (chatRooms[i].users.length < 5) {
+      // Add the user to the room
+      chatRooms[i].users.push(userSocketId);
+      return chatRooms[i];
+    }
+  }
+
+  //   if none of the rooms are available, create a new room
+  const newRoomId = chatRooms.length + 1;
+
+  const newRoom = {
+    id: newRoomId,
+    users: [userSocketId],
+    duration: Math.ceil(Math.random() * 60) * 60,
+  };
+  chatRooms.push(newRoom);
+  return newRoom;
 }
 
-function cleanupRoom(roomName) {
-delete chatRooms[roomName];
-delete roomDurations[roomName];
+function removeFromRoom(socketId) {
+  for (let i = 0; i < chatRooms.length; i++) {
+    if (chatRooms[i].users.includes(socketId)) {
+      chatRooms[i].users = chatRooms[i].users.filter((id) => id !== socketId);
+      //   when last user aka all people leave the room, clean up the room
+      if (chatRooms[i].users.length === 0) {
+        // console.log(`Room ${chatRooms[i].id} cleaned up`);
+        cleanupRoom(chatRooms[i].id);
+      } else {
+        io.to(chatRooms[i].id).emit(
+          "user-left",
+          JSON.stringify({
+            username: socketId,
+            people_in_room: chatRooms[i].users.length,
+            total_online_users: totalOnlineUsersCount(),
+          })
+        );
+      }
+      break;
+    }
+  }
+}
+
+function cleanupRoom(roomId) {
+  chatRooms = chatRooms.filter((room) => room.id !== roomId);
+  // console.log(`Room ${roomId} cleaned up`);
 }
 
 server.listen(port, () => {
-console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
